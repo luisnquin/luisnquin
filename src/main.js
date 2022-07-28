ffbb19a15d6ab179227afcd99d50e08473dafdb5
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-07-28 16:35:56 -0500

    perf: little page optimizations

--- src/main.js
+++ src/main.js
@@ -1,35 +1,36 @@
 'use strict'
 
-const yesterdayDt = new Date().setDate(new Date().getDate() - 1)
-const lastTimeSaved = localStorage.getItem('lastSaveDt')
-let commit
-
-if (lastTimeSaved === null || new Date(lastTimeSaved) < yesterdayDt) {
-	try {
-		commit = fetchLastCommit()
-		commit.then((commit) => {
-			saveCommitInLocalStorage(commit)
-		})
-	} catch (err) {
-		console.error('failed to fetch: ' + err)
-	}
-} else {
-	commit = {
-		lastAuthor: localStorage.getItem('lastAuthor'),
-		lastDate: localStorage.getItem('lastDate')
-	}
+window.onload = () => {
+	loadPageInfo()
+		.then((info) => console.log(info))
+		.catch((err) => console.error(err))
 }
 
-console.log(lastUpdateInfo(commit.lastAuthor, commit.lastDate))
+async function loadPageInfo() {
+	const yesterdayDt = new Date().setDate(new Date().getDate() - 1)
+	const lastTimeSaved = localStorage.getItem('lastSaveDt')
 
-function saveCommitInLocalStorage(commit) {
-	localStorage.setItem('lastAuthor', commit.lastAuthor)
-	localStorage.setItem('lastDate', commit.lastDate)
-	localStorage.setItem('lastSaveDt', new Date().toISOString())
+	if (lastTimeSaved === null || new Date(lastTimeSaved) < yesterdayDt) {
+		try {
+			const commit = await fetchLastCommit()
+			saveCommitInLocalStorage(commit)
+
+			return prettyLastCommit(commit)
+		} catch (err) {
+			throw new Error('failed to fetch: ' + err)
+		}
+	} else {
+		const commit = {
+			lastAuthor: localStorage.getItem('lastAuthor'),
+			lastDate: localStorage.getItem('lastDate')
+		}
+
+		return prettyLastCommit(commit)
+	}
 }
 
 async function fetchLastCommit() {
-	const response = await fetch('https://api.github.com/repos/luisnquin/luisnquin/commits')
+	const response = await fetch('https://api.github.com/repos/luisnquin/luisnquin/commits?per_page=1')
 	if (!response.ok) {
 		throw new Error('response is not ok')
 	} else if (response.status != 200) {
@@ -44,13 +45,19 @@ async function fetchLastCommit() {
 	}
 }
 
-function lastUpdateInfo(author, stringDt) {
-	stringDt = new Date(stringDt).toLocaleDateString('en-US', {
+function saveCommitInLocalStorage({ lastAuthor, lastDate }) {
+	localStorage.setItem('lastAuthor', lastAuthor)
+	localStorage.setItem('lastDate', lastDate)
+	localStorage.setItem('lastSaveDt', new Date().toISOString())
+}
+
+function prettyLastCommit({ lastAuthor, lastDate }) {
+	lastDate = new Date(lastDate).toLocaleDateString('en-US', {
 		weekday: 'long',
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric'
 	})
 
-	return `Repository of page last updated by '${author}' at ${stringDt}. You can visit it in https://github.com/luisnquin/luisnquin/.`
+	return `Repository of page last updated by '${lastAuthor}' at ${lastDate}. You can visit it in https://github.com/luisnquin/luisnquin/.`
 }
