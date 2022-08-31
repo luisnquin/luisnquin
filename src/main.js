224247ecd1afc1fdd488b36486a6793fd4ada050
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2022-08-30 23:07:33 -0500

    feat: eslint + better js code

--- src/main.js
+++ src/main.js
@@ -1,54 +1,63 @@
 'use strict'
 
 window.onload = () => {
-	loadPageInfo()
-		.then((info) => console.log(info))
-		.catch((err) => console.error(err))
+	loadPageInfo().then(console.log).catch(console.error)
 }
 
 async function loadPageInfo() {
-	const yesterdayDt = new Date().setDate(new Date().getDate() - 1)
-	const lastTimeSaved = localStorage.getItem('lastSaveDt')
+	const [yesterdayDt, lastSave] = [
+		new Date().setDate(new Date().getDate() - 1),
+		localStorage.getItem('lastSave'),
+	]
 
-	if (lastTimeSaved === null || new Date(lastTimeSaved) < yesterdayDt) {
+	if (lastSave === null || new Date(lastSave) < yesterdayDt) {
 		try {
-			const commit = await fetchLastCommit()
+			const commit = await getLastCommit()
 			saveCommitInLocalStorage(commit)
 
 			return prettyLastCommit(commit)
 		} catch (err) {
 			throw new Error('failed to fetch: ' + err)
 		}
-	} else {
-		const commit = {
-			lastAuthor: localStorage.getItem('lastAuthor'),
-			lastDate: localStorage.getItem('lastDate')
-		}
-
-		return prettyLastCommit(commit)
 	}
+
+	const [lastAuthor, lastDate] = [
+		localStorage.getItem('lastAuthor'),
+		localStorage.getItem('lastDate'),
+	]
+
+	return prettyLastCommit({
+		lastAuthor: lastAuthor !== null ? lastAuthor : 'luisnquin',
+		lastDate: lastDate !== null ? lastDate : '0000-00-00T00:00:00Z',
+	})
 }
 
-async function fetchLastCommit() {
-	const response = await fetch('https://api.github.com/repos/luisnquin/luisnquin/commits?per_page=1')
+async function getLastCommit() {
+	const response = await fetch(
+		'https://api.github.com/repos/luisnquin/luisnquin/commits?per_page=1'
+	)
 	if (!response.ok) {
 		throw new Error('response is not ok')
 	} else if (response.status != 200) {
 		throw new Error(`unexpected status ${response.status}`)
 	}
 
-	const body = await response.json()
+	const [repo] = await response.json()
+
+	if (repo === null) {
+		throw new Error('unexpected response body')
+	}
 
 	return {
-		lastAuthor: body[0].author.login,
-		lastDate: body[0].commit.committer.date
+		lastAuthor: repo.author.login,
+		lastDate: repo.commit.committer.date,
 	}
 }
 
 function saveCommitInLocalStorage({ lastAuthor, lastDate }) {
 	localStorage.setItem('lastAuthor', lastAuthor)
 	localStorage.setItem('lastDate', lastDate)
-	localStorage.setItem('lastSaveDt', new Date().toISOString())
+	localStorage.setItem('lastSave', new Date().toISOString())
 }
 
 function prettyLastCommit({ lastAuthor, lastDate }) {
@@ -56,7 +65,7 @@ function prettyLastCommit({ lastAuthor, lastDate }) {
 		weekday: 'long',
 		year: 'numeric',
 		month: 'long',
-		day: 'numeric'
+		day: 'numeric',
 	})
 
 	return `Repository of page last updated by '${lastAuthor}' at ${lastDate}. You can visit it in https://github.com/luisnquin/luisnquin/.`
