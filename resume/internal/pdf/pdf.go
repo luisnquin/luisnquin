16058c405af89511921ca185acaf79c62cf02b63
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-03-05 00:49:21 -0500

    refactor(resume.pdf): PDF struct -> Writer struct

--- resume/internal/pdf/pdf.go
+++ resume/internal/pdf/pdf.go
@@ -7,20 +7,20 @@ import (
 	"github.com/luisnquin/luisnquin/resume/internal/pdfutil"
 )
 
-type PDF struct {
+type Writer struct {
 	writer *fpdf.Fpdf
 }
 
-func NewWriter() PDF {
+func NewWriter() Writer {
 	writer := fpdf.New(pdfutil.Portrait, pdfutil.Millimeter, pdfutil.Letter, ".")
 	writer.AddPage()
 
-	return PDF{
+	return Writer{
 		writer: writer,
 	}
 }
 
-func (p PDF) SetLine(x1, y1, x2, y2 float64, opts ...Option) {
+func (w Writer) SetLine(x1, y1, x2, y2 float64, opts ...Option) {
 	var o options
 
 	for _, opt := range opts {
@@ -28,14 +28,14 @@ func (p PDF) SetLine(x1, y1, x2, y2 float64, opts ...Option) {
 	}
 
 	if len(o.drawRGB) == 3 {
-		p.writer.SetDrawColor(o.drawRGB[0], o.drawRGB[1], o.drawRGB[2])
-		defer p.writer.SetDrawColor(0, 0, 0)
+		w.writer.SetDrawColor(o.drawRGB[0], o.drawRGB[1], o.drawRGB[2])
+		defer w.writer.SetDrawColor(0, 0, 0)
 	}
 
-	p.writer.Line(x1, y1, x2, y2)
+	w.writer.Line(x1, y1, x2, y2)
 }
 
-func (p PDF) SetText(text string, x, y float64, opts ...Option) {
+func (w Writer) SetText(text string, x, y float64, opts ...Option) {
 	var o options
 
 	for _, opt := range opts {
@@ -43,18 +43,18 @@ func (p PDF) SetText(text string, x, y float64, opts ...Option) {
 	}
 
 	if o.wordSpacing != 0 {
-		p.writer.SetWordSpacing(o.wordSpacing)
-		defer p.writer.SetWordSpacing(0)
+		w.writer.SetWordSpacing(o.wordSpacing)
+		defer w.writer.SetWordSpacing(0)
 	}
 
 	if len(o.fillRGB) == 3 {
-		p.writer.SetFillColor(o.fillRGB[0], o.fillRGB[1], o.fillRGB[2])
-		defer p.writer.SetFillColor(0, 0, 0)
+		w.writer.SetFillColor(o.fillRGB[0], o.fillRGB[1], o.fillRGB[2])
+		defer w.writer.SetFillColor(0, 0, 0)
 	}
 
 	if len(o.textRGB) == 3 {
-		p.writer.SetTextColor(o.textRGB[0], o.textRGB[1], o.textRGB[2])
-		defer p.writer.SetTextColor(0, 0, 0)
+		w.writer.SetTextColor(o.textRGB[0], o.textRGB[1], o.textRGB[2])
+		defer w.writer.SetTextColor(0, 0, 0)
 	}
 
 	if o.charsLimit == 0 {
@@ -64,22 +64,22 @@ func (p PDF) SetText(text string, x, y float64, opts ...Option) {
 	if len(text) > int(o.charsLimit) {
 		compensation := float64(0)
 
-		for _, line := range p.writer.SplitText(text, o.charsLimit) {
-			p.writer.SetXY(x, y+compensation)
-			p.writer.Cell(0, 0, line)
+		for _, line := range w.writer.SplitText(text, o.charsLimit) {
+			w.writer.SetXY(x, y+compensation)
+			w.writer.Cell(0, 0, line)
 
 			compensation += 5
 		}
 	} else {
-		p.writer.SetXY(x, y)
-		p.writer.Cell(0, 0, text)
+		w.writer.SetXY(x, y)
+		w.writer.Cell(0, 0, text)
 	}
 }
 
-func (p PDF) SetFont(familyStr, styleStr string, size float64) {
-	p.writer.SetFont(familyStr, styleStr, size)
+func (w Writer) SetFont(familyStr, styleStr string, size float64) {
+	w.writer.SetFont(familyStr, styleStr, size)
 }
 
-func (p PDF) OutTo(w io.Writer) error {
+func (p Writer) OutTo(w io.Writer) error {
 	return p.writer.Output(w)
 }
