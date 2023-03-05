a8d6e9340c446b65e014f97cd6d6b35a42ae991d
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-03-05 09:23:31 -0500

    feat(resume.models): more detailed user model compatible with created schema

--- resume/internal/models/user.go
+++ resume/internal/models/user.go
@@ -2,14 +2,30 @@ package models
 
 type (
 	UserInfo struct {
-		FullName        string  `json:"fullName"`
-		SelfDescription string  `json:"selfDescription"`
-		Contact         Contact `json:"contact"`
+		FullName        string          `json:"fullName"`
+		SelfDescription string          `json:"selfDescription"`
+		Contact         Contact         `json:"contact"`
+		HardSkills      []string        `json:"hardSkills"`
+		SoftSkills      []string        `json:"softSkills"`
+		JobExperiences  []JobExperience `json:"jobExperiences"`
+		Hobbies         []Hobby         `json:"hobbies"`
 	}
 
 	Contact struct {
 		Email       string `json:"email"`
 		Web         string `json:"web"`
 		PhoneNumber string `json:"phoneNumber"`
+	}
+
+	JobExperience struct {
+		Position    string `json:"position"`
+		Company     string `json:"company"`
+		FromToLabel string `json:"fromToLabel"`
+		Description string `json:"description"`
+	}
+
+	Hobby struct {
+		Label       string `json:"label"`
+		Description string `json:"description"`
 	}
 )
