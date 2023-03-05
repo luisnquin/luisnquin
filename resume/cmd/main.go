92f2b59246d4913399a0b89510822555e24e716d
Luis Quiñones Requelme <lpaandres2020@gmail.com>
2023-03-05 09:26:10 -0500

    feat(resume): updated cv generation

--- resume/cmd/main.go
+++ resume/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/goccy/go-json"
@@ -10,31 +11,53 @@ import (
 )
 
 func main() {
-	userInfo, err := os.ReadFile("./user_info.json")
-	try(err)
-
-	var user models.UserInfo
-
-	try(json.Unmarshal(userInfo, &user))
-
-	writer := pdf.NewWriter()
-	writer.SetFont("Arial", pdfutil.Bold, 20)
-	writer.SetText(user.FullName, 11, 15) // ! fix
-
-	writer.SetFont("Arial", pdfutil.Regular, 9)
-	writer.SetText(user.Contact.PhoneNumber, 11, 26)
-	writer.SetText(user.Contact.Email, 11, 30)
-	writer.SetText(user.Contact.Web, 11, 34)
-
-	writer.SetText(user.SelfDescription, 10, 43)
-	writer.SetLine(210, 70, 10, 70, pdf.WithDrawColor("#8f8f8f"))
-
 	f, err := os.Create("./build/resume.pdf")
 	try(err)
 
 	defer f.Close()
 
-	try(writer.OutTo(f))
+	userInfo, err := os.ReadFile("./assets/user_info.json")
+	try(err)
+
+	var owner models.UserInfo
+
+	try(json.Unmarshal(userInfo, &owner))
+
+	writer := pdf.NewWriter(f)
+	writer.SetFont("Arial", pdfutil.Bold, 20)
+	writer.SetText(owner.FullName, 11, 15)
+
+	writer.SetFont("Arial", pdfutil.Regular, 9)
+	writer.SetText(owner.Contact.PhoneNumber, 11, 8)
+	writer.SetText(owner.Contact.Email, 11, 5)
+	writer.SetText(owner.Contact.Web, 11, 5, pdf.WithTextColor("#295ee3"), pdf.IsLink())
+
+	writer.SetText(owner.SelfDescription, 10, 10, pdf.WithFontSize(10))
+	writer.SetLine(210, 70, 10, 70, pdf.WithDrawColor("#8f8f8f"))
+
+	writer.SetText("Work experiences", 10, 9, pdf.WithFontSize(14), pdf.WithFontStyle(pdfutil.Bold))
+	writer.AddY(2)
+
+	for _, je := range owner.JobExperiences {
+		writer.SetText(je.Position, 11, 5, pdf.WithFontSize(10), pdf.WithFontStyle(pdfutil.Bold))
+		writer.SetText(fmt.Sprintf("%s %s", je.Company, je.FromToLabel), 11, 4,
+			pdf.WithFontSize(9), pdf.WithFontStyle(pdfutil.Italic), pdf.WithTextColor("#2b2b2b"))
+
+		writer.SetText(je.Description, 10, 5, pdf.WithFontSize(10))
+	}
+
+	writer.SetLine(210, 188, 10, 188, pdf.WithDrawColor("#8f8f8f"))
+
+	writer.SetText("Hobbies", 10, 7, pdf.WithFontSize(13), pdf.WithFontStyle(pdfutil.Bold))
+	writer.AddY(1)
+
+	for _, hobby := range owner.Hobbies {
+		writer.SetText(pdfutil.ToBullet(fmt.Sprintf("%s:", hobby.Label)), 10, 5,
+			pdf.WithFontStyle(pdfutil.Bold, pdfutil.Italic))
+		writer.SetText(hobby.Description, -1, 0, pdf.UsingX())
+	}
+
+	try(writer.Flush())
 }
 
 func try(err error) {
