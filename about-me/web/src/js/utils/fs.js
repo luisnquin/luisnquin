b3782d70d818888641a4a44a37343442b7db95fb
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-17 22:12:00 -0500

    chore: removed about-me/web + moved about-me/cli -> ./cli

--- about-me/web/src/js/utils/fs.js
+++
@@ -1,8 +0,0 @@
-export function bytesToHumanReadableSize(size) {
-  var i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024))
-  return (
-    (size / Math.pow(1024, i)).toFixed(2) * 1 +
-    ' ' +
-    ['B', 'kB', 'MB', 'GB', 'TB'][i]
-  )
-}
