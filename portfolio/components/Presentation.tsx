d7c6e89d3258abedbbc4cc529ed8281d9b70a072
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-19 14:08:04 -0500

    refactor(Presentation): presentation section in its own component

---
+++ portfolio/components/Presentation.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { ScrollDown } from './ScrollDown.tsx'
+import { HeroCommand } from './HeroCommand.jsx'
+import styles from '../styles/Presentation.module.css'
+
+export const Presentation = () => {
+  return (
+    <section id="presentation" className={styles.presentation}>
+      <ScrollDown className={styles.scrollDown} />
+
+      <div>
+        <h1>Luis Quiñones Requelme</h1>
+        <h4>Software Developer</h4>
+      </div>
+
+      <HeroCommand command="npx luisnquin@latest" />
+
+      <ul className={styles.social_media}>
+        <li>
+          <a
+            href="https://github.com/luisnquin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://gitlab.com/luisnquin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitLab
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://linkedin.com/in/luisnquin"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
