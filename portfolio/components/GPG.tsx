44505915e02d923e67576b90e2389ff1b20bd61f
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-21 17:06:25 -0500

    fix(GPG): using textarea + javascript to hide and display public key

--- portfolio/components/GPG.tsx
+++ portfolio/components/GPG.tsx
@@ -1,27 +1,32 @@
 import styles from '../styles/GPG.module.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props {
   publicKey: string
 }
 
 export const GPG = ({ publicKey }: Props): React.JSX.Element => {
+  const [hide, setHide] = useState(true)
+  const handleCheckboxClick = () => setHide(!hide)
+
   return (
     <div className={styles.gpg_container}>
       <input
+        onClick={handleCheckboxClick}
         className={styles.gpg_checkbox_input}
         type="checkbox"
         id="check_id"
       ></input>
       <label className={styles.gpg_checkbox_label} htmlFor="check_id"></label>
 
-      <div className={styles.gpg_public_key}>
-        {publicKey.split('\n').map((line) => {
-          if (!line) return <br />
-
-          return <p>{line}</p>
-        })}
-      </div>
+      <textarea
+        className={styles.gpg_public_key}
+        hidden={hide}
+        disabled
+        readOnly
+        rows={publicKey.split('\n').length}
+        value={publicKey}
+      ></textarea>
     </div>
   )
 }
