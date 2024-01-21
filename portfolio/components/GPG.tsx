54516639d4e062e7c30960e3f73a062ca58c052b
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-21 06:17:00 +0000

    style(actions): ran `npx prettier --write "{*,**/*}.{js,ts,json,tsx,jsx}"`

--- portfolio/components/GPG.tsx
+++ portfolio/components/GPG.tsx
@@ -8,8 +8,12 @@ interface Props {
 export const GPG = ({ publicKey }: Props): React.JSX.Element => {
   return (
     <div className={styles.gpg_container}>
-      <input className={styles.gpg_checkbox_input} type="checkbox" id="check_id"></input>
-      <label  className={styles.gpg_checkbox_label} htmlFor="check_id"></label>
+      <input
+        className={styles.gpg_checkbox_input}
+        type="checkbox"
+        id="check_id"
+      ></input>
+      <label className={styles.gpg_checkbox_label} htmlFor="check_id"></label>
 
       <div className={styles.gpg_public_key}>
         {publicKey.split('\n').map((line) => {
