0a2239090e57b619cf89c4feebb971d4e103a6e7
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-22 00:04:04 -0500

    feat(Footer): excalidraw link + css improvements

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -7,16 +7,30 @@ export const Footer = (): React.JSX.Element => {
     <footer
       className={`${styles.footer} ${firaCode.variable} ${nerdFontsSymbols.variable}`}
     >
-      <span>^^</span>
-      <a
-        href="https://github.com/luisnquin/luisnquin"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="flex-small-gap"
-      >
-        Source
-        <span className={`${styles.external_link} very-small`}> </span>
-      </a>
+      <div>
+        <span>^^</span>
+
+        <div>
+          <a
+            href="https://excalidraw.com/#json=DSx9MC0Lnq3xoc0mGoZoo,hsAHxHw3rt-B6y7EXj0jUg"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-small-gap"
+          >
+            Draft (Excalidraw)
+            <span className={`${styles.external_link} very-small`}> </span>
+          </a>
+          <a
+            href="https://github.com/luisnquin/luisnquin"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-small-gap"
+          >
+            Source
+            <span className={`${styles.external_link} very-small`}> </span>
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
