e9c28364c09ec0d7bbcb51659e66a32474959384
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-21 23:26:08 -0500

    fix(GPG): checkbox inside <label> tag

--- portfolio/components/GPG.tsx
+++ portfolio/components/GPG.tsx
@@ -11,13 +11,14 @@ export const GPG = ({ publicKey }: Props): React.JSX.Element => {
 
   return (
     <div className={styles.gpg_container}>
-      <input
-        onClick={handleCheckboxClick}
-        className={styles.gpg_checkbox_input}
-        type="checkbox"
-        id="check_id"
-      ></input>
-      <label className={styles.gpg_checkbox_label} htmlFor="check_id"></label>
+      <label className={styles.gpg_checkbox_label} htmlFor="check_id">
+        <input
+          onClick={handleCheckboxClick}
+          className={styles.gpg_checkbox_input}
+          type="checkbox"
+          id="check_id"
+        ></input>
+      </label>
 
       <textarea
         className={styles.gpg_public_key}
