e2dfc5362c7f7ebf02a7705c0d10e3050d7e1808
Luis Quiñones <lpaandres2020@gmail.com>
2024-01-22 00:54:42 -0500

    refactor(portfolio): using index.ts to export all components

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -3,14 +3,16 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import data from '../data.json' assert { type: 'json' }
 
-import { Presentation } from '../components/Presentation.tsx'
-import { Technologies } from '../components/Technologies.tsx'
-import { Experience } from '../components/Experience.tsx'
-import { SnakeBoard } from '../components/SnakeBoard.tsx'
-import { ContactMe } from '../components/ContactMe.tsx'
-import { Interests } from '../components/Interests.tsx'
-import { Reviews } from '../components/Reviews.tsx'
-import { Footer } from '../components/Footer.tsx'
+import {
+  Presentation,
+  Technologies,
+  Experience,
+  SnakeBoard,
+  ContactMe,
+  Interests,
+  Reviews,
+  Footer,
+} from '../components'
 
 const {
   gpgPublicKey: publicKey,
